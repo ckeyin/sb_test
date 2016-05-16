57e1b92855bd2d19babe18601f309441c5fe753e
ckeyin <ckeyin@163.com>
2016-05-17 07:13:05 +0800

    war to jar

--- src/main/java/com/dev/spring/AppApplication.java
+++ src/main/java/com/dev/spring/AppApplication.java
@@ -1,26 +1,22 @@
-//// ======================================
-//// Project Name:sb_test
-//// Package Name:com.dev.spring
-//// File Name:AppApplication.java
-//// Created Data:2016/5/6 11:10
-//// ======================================
-//package com.dev.spring;
-//
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-//import org.springframework.context.annotation.ComponentScan;
-//import org.springframework.context.annotation.Configuration;
-//
-///**
-// * Created by cky on 2016/5/6 11:10.
-// */
-//@Configuration
-//@EnableAutoConfiguration
-//@ComponentScan
-//public class AppApplication {
-//
-//    public static void main(String[] args) {
-//        SpringApplication.run(AppApplication.class, args);
-//    }
-//
-//}
+// ======================================
+// Project Name:sb_test
+// Package Name:com.dev.spring
+// File Name:AppApplication.java
+// Created Data:2016/5/6 11:10
+// ======================================
+package com.dev.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by cky on 2016/5/6 11:10.
+ */
+@SpringBootApplication
+public class AppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppApplication.class, args);
+    }
+
+}
