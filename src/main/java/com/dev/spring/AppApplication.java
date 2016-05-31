a8f44d2084835a63e7d3ffc814a25bf21e37c64f
ckeyin <ckeyin@163.com>
2016-05-31 18:08:31 +0800

    war jsp

--- src/main/java/com/dev/spring/AppApplication.java
+++ src/main/java/com/dev/spring/AppApplication.java
@@ -1,42 +1,42 @@
-// ======================================
-// Project Name:sb_test
-// Package Name:com.dev.spring
-// File Name:AppApplication.java
-// Created Data:2016/5/6 11:10
-// ======================================
-package com.dev.spring;
-
-import com.dev.spring.config.MyApplicationEnvironmentPreparedEventListener;
-import com.dev.spring.config.MyApplicationFailedEventListener;
-import com.dev.spring.config.MyApplicationPreparedEventListener;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
-import org.springframework.context.annotation.Bean;
-
-import java.io.File;
-import java.util.concurrent.TimeUnit;
-
-/**
- * Created by cky on 2016/5/6 11:10.
- */
-@SpringBootApplication
-public class AppApplication {
-
-    @Bean
-    public EmbeddedServletContainerCustomizer embeddedServletContainerCustomizer() {
-        return (ConfigurableEmbeddedServletContainer container) -> {
-            container.setDocumentRoot(new File("/jsp"));
-        };
-    }
-
-    public static void main(String[] args) {
-
-        SpringApplication application = new SpringApplication(AppApplication.class);
-        application.addListeners(new MyApplicationEnvironmentPreparedEventListener(), new
-                MyApplicationPreparedEventListener(), new MyApplicationFailedEventListener());
-        application.run(args);
-    }
-
-}
+//// ======================================
+//// Project Name:sb_test
+//// Package Name:com.dev.spring
+//// File Name:AppApplication.java
+//// Created Data:2016/5/6 11:10
+//// ======================================
+//package com.dev.spring;
+//
+//import com.dev.spring.config.MyApplicationEnvironmentPreparedEventListener;
+//import com.dev.spring.config.MyApplicationFailedEventListener;
+//import com.dev.spring.config.MyApplicationPreparedEventListener;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+//import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+//import org.springframework.context.annotation.Bean;
+//
+//import java.io.File;
+//import java.util.concurrent.TimeUnit;
+//
+///**
+// * Created by cky on 2016/5/6 11:10.
+// */
+//@SpringBootApplication
+//public class AppApplication {
+//
+//    @Bean
+//    public EmbeddedServletContainerCustomizer embeddedServletContainerCustomizer() {
+//        return (ConfigurableEmbeddedServletContainer container) -> {
+//            container.setDocumentRoot(new File("src/main/resources"));
+//        };
+//    }
+//
+//    public static void main(String[] args) {
+//
+//        SpringApplication application = new SpringApplication(AppApplication.class);
+//        application.addListeners(new MyApplicationEnvironmentPreparedEventListener(), new
+//                MyApplicationPreparedEventListener(), new MyApplicationFailedEventListener());
+//        application.run(args);
+//    }
+//
+//}
--- src/main/java/com/dev/spring/WebJspApplication.java
+++ src/main/java/com/dev/spring/WebJspApplication.java
@@ -1,28 +1,35 @@
-//// ======================================
-//// Project Name:sb_test
-//// Package Name:com.dev.spring
-//// File Name:WebJspApplication.java
-//// Created Data:2016/5/6 16:12
-//// ======================================
-//package com.dev.spring;
-//
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.boot.builder.SpringApplicationBuilder;
-//import org.springframework.boot.web.support.SpringBootServletInitializer;
-//
-///**
-// * Created by cky on 2016/5/6 16:12.
-// */
-//@SpringBootApplication
-//public class WebJspApplication extends SpringBootServletInitializer {
-//
-//    @Override
-//    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-//        return builder.sources(WebJspApplication.class);
+// ======================================
+// Project Name:sb_test
+// Package Name:com.dev.spring
+// File Name:WebJspApplication.java
+// Created Data:2016/5/6 16:12
+// ======================================
+package com.dev.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+/**
+ * Created by cky on 2016/5/6 16:12.
+ */
+@SpringBootApplication
+public class WebJspApplication extends SpringBootServletInitializer {
+
+//    @Bean
+//    public EmbeddedServletContainerCustomizer embeddedServletContainerCustomizer() {
+//        return (ConfigurableEmbeddedServletContainer container) -> {
+//            container.setDocumentRoot(new File("src/main/resources"));
+//        };
 //    }
-//
-//    public static void main(String[] args) {
-//        SpringApplication.run(WebJspApplication.class, args);
-//    }
-//}
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(WebJspApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebJspApplication.class, args);
+    }
+}
